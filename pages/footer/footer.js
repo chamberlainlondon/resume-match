41e0189ae6e01a2f8327f1340a4c45aec19810e3
London Chamberlain <149753068+chamberlainlondon@users.noreply.github.com>
2025-02-24 17:14:22 -0600

    Update footer.js

--- pages/footer/footer.js
+++ pages/footer/footer.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("footer.html")
+    let footerPath = "footer/footer.html"; // Default path for index.html
+
+    // Adjust the path if the script is running from pages/tool.html
+    if (window.location.pathname.includes("/pages/")) {
+        footerPath = "../footer/footer.html";
+    }
+
+    fetch(footerPath)
         .then(response => response.text())
         .then(data => {
             document.body.insertAdjacentHTML("beforeend", data);
-        });
+        })
+        .catch(error => console.error("Error loading footer:", error));
 });
